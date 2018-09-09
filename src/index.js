708097355004e3564971c8ee73626fa4182a94e0
Andrii Demianenko <andruxond98@gmail.com>
2018-09-09 15:20:44 +0300

    fix1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/sign-in.css'
 import './css/companies.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
-import sendingData from './js/signIn'
+import sendingData from './js/signUp'
 import renderCompanies from './js/companies'
 
 
